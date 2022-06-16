8812f2e0cbf463b22c6d6f9666dbae896121fe85
nikki calamia <Ncalamia2468@yahoo.com>
2022-06-16 18:29:51 -0400

    fixing master code

--- src/App.js
+++ src/App.js
@@ -84,52 +84,6 @@ const App = () => {
     }
   }, [])
 
-///////Functions//////////
-const handleCreate = (add)=>{
-  axios.post(herokuURL, add, {headers: {
-    'content-type': 'multipart/form-data'
-  }}).then((response)=>{
-    console.log(response)
-    console.log(response.data.id)
-    setProducts([...products, response.data])
-
-  })
-}
-
-    /////////////////DELETE///////////////////////////////
-    const handleDelete = (event, deleted) => {
-      axios
-        .delete(herokuURL + '/' + event.target.value)
-        .then((response) => {
-          getProducts()
-
-        })
-    }
-/////////////////Update///////////////////////////////
-const handleUpdate = (updateProduct) => {
-    console.log(updateProduct.id)
-  axios
-    .put(herokuURL +'/' + updateProduct.id, updateProduct)
-    .then((response) => {
-      getProducts()
-      setProducts(products.map((product)=>{
-        return product.id !=response.data.id ? product : response.data
-      }))
-
-    })
-}
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
   //////////////////////////////////////////////
   // the return - skeleton
   //////////////////////////////////////////////
@@ -147,11 +101,6 @@ function Copyright() {
 
         <Login view={view} setView={setView} />
 
-const theme = createTheme({
-  palette: {
-    primary: {
-
-      main: '#ff4400',
       </>
     )
   } else if (view === 'signup') {
@@ -169,126 +118,6 @@ const theme = createTheme({
       </>
     )
   }
-})
-  return (
-    <>
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <AppBar position="relative">
-      <Toolbar>
-
-        <Typography variant="h6" color="inherit" noWrap>
-          Home.
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <main>
-      {/* Hero unit */}
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          pt: 8,
-          pb: 6,
-        }}
-      >
-        <Button variant="outlined"><Add handleCreate={handleCreate}/></Button>
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Essentials.
-          </Typography>
-          <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            Welcome!
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained">Browse Categories</Button>
-
-          </Stack>
-        </Container>
-      </Box>
-      <Container sx={{ py: 8 }} maxWidth="md">
-        {/* End hero unit */}
-        <Grid container spacing={4}>
-          {products.map((product) => (
-            <Grid item key={product} xs={12} sm={6} md={4}>
-              <Card
-                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-              >
-                <CardMedia
-                  component="img"
-                  sx={{
-                    // 16:9
-                    pt: '56.25%',
-                  }}
-                  image={product.image}
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                  {product.name}
-                  </Typography>
-                  <Typography>
-                    Price: {product.price}$
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                <Button onClick={handleDelete} value={product.id}>Delete</Button>
-                  <Edit handleUpdate={handleUpdate} id={product.id}/>
-                </CardActions>
-              </Card>
-
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </main>
-    {/* Footer */}
-    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-      <Typography variant="h6" align="center" gutterBottom>
-
-      </Typography>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="text.secondary"
-        component="p"
-      >
-        Created by Nikki, Jacqueline and Yulia.
-      </Typography>
-      <Copyright />
-    </Box>
-
-  </ThemeProvider>
-  </>
-    // <div className='container'>
-    //   <div >
-    //   <header>HOME</header>
-    //   </div>
-    // <Add handleCreate={handleCreate}/>
-    //   {products.map((product) => {
-    //     return (
-    //       <div className="product" key={product.id}>
-    //         <h4>Name: {product.name}</h4>
-    //         <img src={product.image}/>
-    //         <h4>Category: {product.category}</h4>
-    //         <h4>Price: {product.price}</h4>
-    //         <Edit handleUpdate={handleUpdate} id={product.id}/>
-    //         <button onClick={handleDelete} value={product.id}>DELETE</button>
-    //       </div>
-    //     )
-    //   })}
-    // </div>
-  )
 }
 
 export default App;
